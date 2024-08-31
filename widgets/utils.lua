7bc266b1dfc07a078b9363101ed835d21e8e15cb
dzl71 <davidzlatin406@gmail.com>
2024-08-31 20:41:02 +0300

    added widget_init function, and updated the widget_base structure

--- widgets/utils.lua
+++ widgets/utils.lua
@@ -3,24 +3,24 @@ local gears = require "gears"
 
 local utils = {}
 
-function utils.widget_base(color, left_margin, right_margin, max_width)
+function utils.widget_base()
 	return wibox.widget {
+		id = "constraint",
+		widget = wibox.container.constraint,
+		strategy = "max",
+		-- default_bg = color,
 		{
+			id = 'background',
+			widget = wibox.container.background,
+			shape = gears.shape.rounded_bar,
+			-- bg = color,
 			{
 				id = 'margin',
 				widget = wibox.container.margin,
-				left = left_margin,
-				right = right_margin
+				-- left = left_margin,
+				-- right = right_margin,
 			},
-			id = 'background',
-			widget = wibox.container.background,
-			bg = color,
-			shape = gears.shape.rounded_bar,
 		},
-		default_bg = color,
-		widget = wibox.container.constraint,
-		strategy = "max",
-		width = max_width
 	}
 end
 
@@ -30,6 +30,18 @@ end
 
 function utils.inject_info(parent_widget, child_widget)
 	parent_widget:get_children_by_id("margin")[1].widget = child_widget
+end
+
+---initialize the widget base
+---@param color string
+---@param left_margin number?
+---@param right_margin number?
+function utils.widget_init(widget, color, left_margin, right_margin)
+	widget:get_children_by_id("margin")[1].left = left_margin
+	widget:get_children_by_id("margin")[1].right = right_margin
+	widget:get_children_by_id("background")[1].bg = color
+	widget:get_children_by_id("constraint")[1].default_bg = color
+	-- widget:get_children_by_id("constraint")[1].max_width = max_width
 end
 
 return utils
